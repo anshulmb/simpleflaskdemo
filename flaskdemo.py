8b4c0a2df6deacdf264dcfbdfef0d6e882ba534b
anshulmb <anshultiwarimb@gmail.com>
2025-02-17 19:00:48 +0530

    comm2

--- flaskdemo.py
+++ flaskdemo.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello flask from Python Developer!!!'
+    return 'Hello flask from Python Developer!!'
 
 app.run(host='0.0.0.0', port=81)
